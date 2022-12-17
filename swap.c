9ffef9a7b0317f35ecf36f2b4a528772b16ca69e
Tusingura-rebecca <rtusingura@gmail.com>
2022-12-17 13:03:32 -0800

    more_files

--- swap.c
+++ swap.c
@@ -17,6 +17,14 @@ void f_swap(stack_t **head, unsigned int counter)
 		h = h->next;
 		len++;
 	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 	h = *head;
 	aux = h->n;
 	h->n = h->next->n;
